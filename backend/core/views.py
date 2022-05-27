2bd4d94b8c218622ce1fc6cd201e578d4fd63420
Dmitry Zhdanov <signmark@gmail.com>
2022-05-27 20:50:00 +0300

    Initial commit

---
+++ backend/core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class index(APIView):
+    def get(self, request, format=None):
+
+        return Response('Hello World!')
